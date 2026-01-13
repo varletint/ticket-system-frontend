170975a8a9a92c2e0aa39fdabbbfbd78f8fdb09e
varletint <intvarlet@gmail.com>
2026-01-13 20:47:51 +0100

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_BASE_URL || "/api/v1";
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 console.log(API_URL);
 
 // Create axios instance
